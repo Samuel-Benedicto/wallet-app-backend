56009dea79486f4b7d26b9b4156b9f8a2ec77e1e
design.ecommerce@gmail.com <design.ecommerce30@gmail.com>
2025-03-12 11:08:35 -0300

    change in host port

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const db = require("./db");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Olá, essa é a aplicação Wallet App! ");
+  res.send("Olá, essa é a aplicação Wallet App!");
 });
 
 app.get("/categories", (req, res) => {
